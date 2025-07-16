19b130225632d3c862a5573ec1b671b7a79d8de7
Pavithr Jain <pavithrjain19@gmail.com>
2025-07-16 20:07:04 +0530

    022

---
+++ src/components/BlogCard.jsx
@@ -0,0 +1,31 @@
+
+const truncateText = (text = "", limit = 100) => {
+    return text.length > limit ? text.slice(0, limit).trim() + "..." : text;
+};
+
+
+const BlogCard = ({ title, excerpt, author, date }) => {
+    return (
+        // <div className="flex flex-col justify-between p-6 text-black transition duration-300 bg-white shadow rounded-xl hover:shadow-lg">
+
+        <div className="p-6 text-black transition duration-300 bg-white shadow rounded-xl hover:shadow-lg">
+            <h2 className="text-xl font-semibold">
+                {truncateText(title, 50)}
+            </h2>
+
+            <p className="mt-1 text-sm text-gray-500">
+                By {author} • {date}
+            </p>
+
+            <p className="mt-3 text-gray-700">
+                {truncateText(excerpt, 80)}
+            </p>
+
+            <button className="mt-4 font-medium text-orange-500 hover:underline">
+                Read More →
+            </button>
+        </div>
+    )
+};
+
+export default BlogCard;
