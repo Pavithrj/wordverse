ba18be8a4ce30f5ac16f2c3e1e53032202e013ef
Pavithr Jain <pavithrjain19@gmail.com>
2025-07-20 13:16:17 +0530

    030

---
+++ src/data/Testimonials.js
@@ -0,0 +1,24 @@
+import Car from './../assets/car.jpg';
+
+const Testimonials = [
+    {
+        name: "Sarah Doe",
+        quote: "WordVerse changed how I express my ideas!",
+        role: "Content Creator",
+        image: Car
+    },
+    {
+        name: "John Smith",
+        quote: "The platform is super intuitive and inspiring.",
+        role: "Freelance Writer",
+        image: Car
+    },
+    {
+        name: "Aisha Khan",
+        quote: "I found my writing voice on WordVerse!",
+        role: "Blogger",
+        image: Car
+    }
+];
+
+export default Testimonials;
