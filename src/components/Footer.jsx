52c408b72317dea93ea78a466a19130f0df4b6fb
Pavithr Jain <pavithrjain19@gmail.com>
2025-07-20 13:17:09 +0530

    032

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+    return (
+        <footer className="py-6 text-sm text-center text-gray-500 bg-white border-t border-gray-200">
+            © {new Date().getFullYear()} WordVerse. All rights reserved.
+        </footer>
+    )
+};
+
+export default Footer;
