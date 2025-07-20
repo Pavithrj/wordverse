862c5a85381236a0bcc0b5056d8edf1606401c8d
Pavithr Jain <pavithrjain19@gmail.com>
2025-07-20 13:15:04 +0530

    029

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,9 +1,31 @@
+import AboutUsHero from '../components/aboutUs/AboutUsHero';
+import OurStory from '../components/aboutUs/OurStory';
+import MissionVision from '../components/aboutUs/MissionVision';
+import CoreValues from '../components/aboutUs/CoreValues';
+import MeetTheCreator from '../components/aboutUs/MeetTheCreator';
+import PlatformStats from '../components/aboutUs/PlatformStats';
+import ContactCTA from '../components/aboutUs/ContactCTA';
+import Footer from '../components/Footer';
 
 const AboutUs = () => {
     return (
-        <div>
-            About Us
-        </div>
+        <>
+            <AboutUsHero />
+
+            <OurStory />
+
+            <MissionVision />
+
+            <CoreValues />
+
+            <MeetTheCreator />
+
+            <PlatformStats />
+
+            <ContactCTA />
+
+            <Footer />
+        </>
     )
 };
 
