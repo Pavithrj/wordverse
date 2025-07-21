330fa8c3f0a8a4f5441d5b1bf3ef27e3c742b77b
Pavithr Jain <pavithrjain19@gmail.com>
2025-07-21 09:08:10 +0530

    052

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
-import Services from './pages/Services';
 import About from './pages/AboutUs';
 import Blogs from './pages/Blogs';
+import BlogDetail from './pages/BlogDetail';
 import Contact from './pages/ContactUs';
+import Footer from './components/Footer';
 
 function App() {
     return (
         <Router>
-            <Navbar />
+            <div className="fixed w-full">
+                <Navbar />
+            </div>
 
             <Routes>
                 <Route path="/wordverse" element={<Home />} />
-                <Route path="/wordverse/services" element={<Services />} />
                 <Route path="/wordverse/about" element={<About />} />
                 <Route path="/wordverse/blogs" element={<Blogs />} />
+                <Route path="/wordverse/blogs/:slug" element={<BlogDetail />} />
                 <Route path="/wordverse/contact" element={<Contact />} />
             </Routes>
+
+            <Footer />
         </Router>
     )
 };
