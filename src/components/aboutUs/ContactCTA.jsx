fd2ea2204e68f1663a2ea3a286d1d1d5f6e661f0
Pavithr Jain <pavithrjain19@gmail.com>
2025-07-21 09:19:18 +0530

    071

--- src/components/aboutUs/ContactCTA.jsx
+++ src/components/aboutUs/ContactCTA.jsx
@@ -1,9 +1,15 @@
 
 const ContactCTA = () => (
     <section className="py-16 text-center bg-gray-100">
-        <h2 className="mb-4 text-3xl font-bold">Want to connect?</h2>
-        <p className="mb-6 text-gray-600">Have feedback, questions, or ideas? Let’s talk!</p>
-        <a href="/contact" className="px-6 py-3 text-white transition bg-indigo-600 rounded-full hover:bg-indigo-700">
+        <h2 className="mb-4 text-3xl font-bold">
+            Want to connect?
+        </h2>
+
+        <p className="mb-6 text-gray-600">
+            Have feedback, questions, or ideas? Let’s talk!
+        </p>
+
+        <a href="/wordverse/contact" className="px-6 py-3 text-white transition bg-indigo-600 rounded-full hover:bg-indigo-700">
             Contact Us
         </a>
     </section>
