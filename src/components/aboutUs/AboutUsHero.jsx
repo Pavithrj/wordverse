48d9f128897630edec1001abc99130e1479d8a5d
Pavithr Jain <pavithrjain19@gmail.com>
2025-07-21 09:19:49 +0530

    072

--- src/components/aboutUs/AboutUsHero.jsx
+++ src/components/aboutUs/AboutUsHero.jsx
@@ -1,9 +1,14 @@
 
-const AboutUsHero = () => (
-    <section className="py-20 text-center text-white bg-gradient-to-r from-purple-500 to-indigo-600">
-        <h1 className="mb-4 text-5xl font-bold">About WordVerse</h1>
-        <p className="max-w-2xl mx-auto text-lg">Empowering writers and storytellers through intuitive design and community-driven blogging.</p>
+const AboutHero = () => (
+    <section className="flex flex-col items-center justify-center min-h-screen gap-4 px-6 py-8 mx-auto text-center max-w-7xl">
+        <h1 className="text-2xl font-bold leading-tight sm:text-4xl md:text-6xl">
+            About Word<span className="text-orange-500">Verse</span>
+        </h1>
+
+        <p className="max-w-2xl mx-auto text-lg">
+            Empowering writers and storytellers through intuitive design and community-driven blogging.
+        </p>
     </section>
 );
 
-export default AboutUsHero;
+export default AboutHero;
