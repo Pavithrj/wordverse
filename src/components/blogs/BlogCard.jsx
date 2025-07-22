8ba6df56a5c43a54112b63b769fff9e6db3485b7
Pavithr Jain <pavithrjain19@gmail.com>
2025-07-22 08:09:19 +0530

    088

--- src/components/blogs/BlogCard.jsx
+++ src/components/blogs/BlogCard.jsx
@@ -5,14 +5,23 @@ const BlogCard = ({ blog }) => {
         <Link to={`/wordverse/blogs/${blog.slug}`} className="block">
             <div className="overflow-hidden transition bg-white rounded-lg shadow hover:shadow-md">
                 <img src={blog.image} alt={blog.title} className="object-cover w-full h-48" />
+
                 <div className="p-4">
-                    <h2 className="text-xl font-semibold">{blog.title}</h2>
-                    <p className="mt-1 text-sm text-gray-500">{blog.author} • {blog.date}</p>
-                    <p className="mt-3 text-gray-700">{blog.excerpt}</p>
+                    <h2 className="text-xl font-semibold">
+                        {blog.title}
+                    </h2>
+
+                    <p className="mt-1 text-sm text-gray-500">
+                        {blog.author} • {blog.date}
+                    </p>
+
+                    <p className="mt-3 text-gray-700">
+                        {blog.excerpt}
+                    </p>
                 </div>
             </div>
         </Link>
-    );
+    )
 };
 
 export default BlogCard;
