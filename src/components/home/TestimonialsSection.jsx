7570baee5d748af6885366863834d531435fec1f
Pavithr Jain <pavithrjain19@gmail.com>
2025-07-22 08:04:59 +0530

    082

--- src/components/home/TestimonialsSection.jsx
+++ src/components/home/TestimonialsSection.jsx
@@ -4,7 +4,9 @@ const TestimonialsSection = () => {
     return (
         <section className="px-6 py-16 text-black bg-gray-50">
             <div className="max-w-5xl mx-auto text-center">
-                <h2 className="mb-10 text-2xl font-bold">What Our Readers Say</h2>
+                <h2 className="mb-10 text-2xl font-bold">
+                    What Our Readers Say
+                </h2>
 
                 <div className="grid gap-8 sm:grid-cols-2 md:grid-cols-3">
                     {Testimonials.map(({ name, quote, role, image }, index) => (
