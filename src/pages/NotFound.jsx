821896403a62b65e4e82a15f2145321de46ba41e
Pavithr Jain <pavithrjain19@gmail.com>
2025-07-22 08:00:40 +0530

    077

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+
+const NotFound = () => {
+    return (
+        <div className="py-20 text-center">
+            <h1 className="mb-4 text-4xl font-bold text-red-500">404</h1>
+            <p className="text-lg text-gray-600">Page Not Found</p>
+        </div>
+    )
+};
+
+export default NotFound;
