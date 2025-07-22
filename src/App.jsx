4949663b087a02d95ad882af14648f884d1375eb
Pavithr Jain <pavithrjain19@gmail.com>
2025-07-22 07:59:56 +0530

    075

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,43 @@
+import { lazy } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import About from './pages/AboutUs';
-import Blogs from './pages/Blogs';
-import BlogDetail from './pages/BlogDetail';
-import Contact from './pages/ContactUs';
 import Footer from './components/Footer';
+
+import NotFound from './pages/NotFound';
+
+const Home = lazy(() => import('./pages/Home'));
+const AboutUs = lazy(() => import('./pages/AboutUs'));
+const Blogs = lazy(() => import('./pages/Blogs'));
+const BlogDetail = lazy(() => import('./pages/BlogDetail'));
+const ContactUs = lazy(() => import('./pages/ContactUs'));
+
+const routes = [
+    { path: '/wordverse', element: <Home /> },
+    { path: '/wordverse/about', element: <AboutUs /> },
+    { path: '/wordverse/blogs', element: <Blogs /> },
+    { path: '/wordverse/blogs/:slug', element: <BlogDetail /> },
+    { path: '/wordverse/contact', element: <ContactUs /> }
+];
 
 function App() {
     return (
         <Router>
-            <div className="fixed w-full">
+            <div className="flex flex-col min-h-screen">
                 <Navbar />
+
+                <main className="flex-grow">
+                    <Routes>
+                        {routes.map(({ index, path, element }) => (
+                            <Route key={index} path={path} element={element} />
+                        ))}
+
+                        <Route path="*" element={<NotFound />} />
+                    </Routes>
+                </main>
+
+                <Footer />
             </div>
-
-            <Routes>
-                <Route path="/wordverse" element={<Home />} />
-                <Route path="/wordverse/about" element={<About />} />
-                <Route path="/wordverse/blogs" element={<Blogs />} />
-                <Route path="/wordverse/blogs/:slug" element={<BlogDetail />} />
-                <Route path="/wordverse/contact" element={<Contact />} />
-            </Routes>
-
-            <Footer />
         </Router>
     )
 };
