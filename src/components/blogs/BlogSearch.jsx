179bc20c878d8da0967a41a12b284d66bb7baad6
Pavithr Jain <pavithrjain19@gmail.com>
2025-07-22 08:07:16 +0530

    085

--- src/components/blogs/BlogSearch.jsx
+++ src/components/blogs/BlogSearch.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 const BlogSearch = ({ onSearch }) => {
-    const [query, setQuery] = useState('');
+    const [query, setQuery] = useState("");
 
     const handleSearch = () => {
         onSearch(query);
@@ -9,21 +9,13 @@ const BlogSearch = ({ onSearch }) => {
 
     return (
         <div className="flex gap-2 my-6">
-            <input
-                type="text"
-                placeholder="Search blogs..."
-                value={query}
-                onChange={(e) => setQuery(e.target.value)}
-                className="w-full p-2 border border-gray-300 rounded-md"
-            />
-            <button
-                onClick={handleSearch}
-                className="px-4 py-2 text-white transition bg-indigo-600 rounded-md hover:bg-indigo-700"
-            >
+            <input type="text" placeholder="Search blogs..." value={query} onChange={(e) => setQuery(e.target.value)} className="w-full p-2 border border-gray-300 rounded-md" />
+
+            <button onClick={handleSearch} className="px-4 py-2 text-white transition bg-indigo-600 rounded-md hover:bg-indigo-700">
                 Search
             </button>
         </div>
-    );
+    )
 };
 
 export default BlogSearch;
