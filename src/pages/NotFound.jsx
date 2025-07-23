4d07433697bfde373126e04fe654fc2df519a4de
Pavithr jain <68775236+Pavithrj@users.noreply.github.com>
2025-07-23 07:10:14 +0000

    092

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,8 +2,13 @@
 const NotFound = () => {
     return (
         <div className="py-20 text-center">
-            <h1 className="mb-4 text-4xl font-bold text-red-500">404</h1>
-            <p className="text-lg text-gray-600">Page Not Found</p>
+            <h1 className="mb-4 text-4xl font-bold text-red-500">
+                404
+            </h1>
+
+            <p className="text-lg text-gray-600">
+                Page Not Found
+            </p>
         </div>
     )
 };
