7f5ff5e959c1cc1d85be69575418515ae4b0943e
Pavithr jain <68775236+Pavithrj@users.noreply.github.com>
2025-07-24 10:03:27 +0000

    120

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
     }, [isOpen]);
 
     return (
-        <div className="sticky top-0 z-50 text-white bg-black shadow-md">
+        <div className="fixed top-0 left-0 w-full z-50 bg-black text-white shadow-md">
             <nav className="flex items-center justify-between px-6 py-4 mx-auto max-w-7xl">
                 <NavLink to="/wordverse" className="text-2xl font-extrabold">
                     <span className="text-white">
