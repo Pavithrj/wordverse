4887c16e1eb7441db156353f180516ed5620e23f
Pavithr Jain <pavithrjain19@gmail.com>
2025-07-24 08:53:58 +0530

    037

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,14 +1,23 @@
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
     return (
-        <div className="py-20 text-center">
-            <h1 className="mb-4 text-4xl font-bold text-red-500">
-                404
-            </h1>
+        <div className="flex items-center justify-center min-h-[70vh] bg-gray-100 px-4">
+            <div className="text-center animate-fadeIn">
+                <h1 className="mb-4 text-6xl font-bold text-gray-800">
+                    404
+                </h1>
 
-            <p className="text-lg text-gray-600">
-                Page Not Found
-            </p>
+                <p className="mb-6 text-gray-600 text-md xl:text-xl">
+                    Oops! The page you're looking for doesn't exist.
+                </p>
+
+                <Link to="/wordverse">
+                    <button className="px-6 py-3 text-white transition duration-300 bg-orange-500 shadow w-fit rounded-xl hover:bg-orange-600">
+                        Go Back Home
+                    </button>
+                </Link>
+            </div>
         </div>
     )
 };
