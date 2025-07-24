53896eaf6d85e547e19a4a353f8e89e9267e200c
Pavithr jain <68775236+Pavithrj@users.noreply.github.com>
2025-07-24 07:15:48 +0000

    105

--- src/components/aboutUs/AboutUsHero.jsx
+++ src/components/aboutUs/AboutUsHero.jsx
@@ -1,6 +1,6 @@
 
-const AboutHero = () => (
-    <section className="flex flex-col items-center justify-center min-h-screen gap-4 px-6 py-8 mx-auto text-center max-w-7xl">
+const AboutUsHero = () => (
+    <section className="flex flex-col items-center justify-center min-h-screen gap-4 px-6 mx-auto text-center max-w-7xl">
         <h1 className="text-2xl font-bold leading-tight sm:text-4xl md:text-6xl">
             About Word<span className="text-orange-500">Verse</span>
         </h1>
@@ -11,4 +11,4 @@ const AboutHero = () => (
     </section>
 );
 
-export default AboutHero;
+export default AboutUsHero;
