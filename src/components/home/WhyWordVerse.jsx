fc9c8ec7cdb97c468b92b0db4c6c8529ef6eea48
Pavithr jain <68775236+Pavithrj@users.noreply.github.com>
2025-07-24 10:01:07 +0000

    115

--- src/components/home/WhyWordVerse.jsx
+++ src/components/home/WhyWordVerse.jsx
@@ -3,16 +3,16 @@ import { Link } from 'react-router-dom';
 const WhyWordVerse = () => {
     return (
         <section className="px-6 py-16 text-black bg-white">
-            <div className="max-w-5xl mx-auto text-center">
-                <h2 className="mb-4 text-3xl font-bold">
+            <div className="max-w-5xl mx-auto text-center items-center flex flex-col gap-4">
+                <h2 className="text-2xl font-bold text-center text-black sm:text-3xl">
                     Why WordVerse?
                 </h2>
 
-                <p className="mb-6 text-lg text-gray-600">
+                <p className="text-md lg:text-lg text-gray-600">
                     WordVerse isn’t just a blog — it’s a canvas for every storyteller, thinker, and creator. Share your experiences, explore new voices, and shape your digital footprint.
                 </p>
 
-                <Link to="/wordverse/about" className="inline-block px-5 py-2 text-orange-500 transition border border-orange-500 rounded-full hover:bg-orange-500 hover:text-white">
+                <Link to="/wordverse/about" className="inline-block px-5 py-2 text-orange-500 transition border border-orange-500 rounded-full hover:bg-orange-500 hover:text-white w-fit">
                     Learn More
                 </Link>
             </div>
