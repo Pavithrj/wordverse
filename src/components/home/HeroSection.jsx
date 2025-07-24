89eb06cfac925b68209f1197f734249c37fe8885
Pavithr jain <68775236+Pavithrj@users.noreply.github.com>
2025-07-24 07:15:10 +0000

    103

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 const HeroSection = () => {
     return (
-        <section className="flex flex-col items-center justify-center min-h-screen gap-4 px-6 py-8 mx-auto text-center max-w-7xl">
+        <header className="flex flex-col items-center justify-center min-h-screen gap-4 px-6 py-8 mx-auto text-center max-w-7xl">
             <h1 className="text-2xl font-bold leading-tight sm:text-4xl md:text-6xl">
                 Welcome to Word<span className="text-orange-500">Verse</span>
             </h1>
@@ -14,7 +14,7 @@ const HeroSection = () => {
             <Link to="/wordverse/blogs" className="px-6 py-3 text-white transition duration-300 bg-orange-500 shadow w-fit rounded-xl hover:bg-orange-600">
                 Explore Blogs
             </Link>
-        </section>
+        </header>
     )
 };
 
