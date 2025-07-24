4c82e66c0a1c9089fb8953f9c527cda6980895ac
Pavithr jain <68775236+Pavithrj@users.noreply.github.com>
2025-07-24 10:02:48 +0000

    118

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 const HeroSection = () => {
     return (
-        <header className="flex flex-col items-center justify-center min-h-screen gap-4 px-6 py-8 mx-auto text-center max-w-7xl">
+        <header className="flex flex-col items-center justify-center min-h-screen gap-4 px-6 py-16 mx-auto text-center max-w-7xl">
             <h1 className="text-2xl font-bold leading-tight sm:text-4xl md:text-6xl">
                 Welcome to Word<span className="text-orange-500">Verse</span>
             </h1>
